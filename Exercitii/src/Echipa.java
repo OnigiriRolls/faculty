0313f33d9609c857c9c655fea07aeefd235bedee
Onigiri <kethrinlie@yahoo.com>
2021-12-02 21:02:24 +0200

    done

--- Exercitii/src/Echipa.java
+++ Exercitii/src/Echipa.java
@@ -1,62 +1,60 @@
 import java.util.ArrayList;
 
 public abstract class Echipa {
-    private Membru lider;
-    private ArrayList<Membru> membri = new ArrayList<Membru>();
+    protected Membru lider;
+    protected ArrayList<Membru> membri = new ArrayList<Membru>();
     private String nume;
     private int MAX;
     private int nr;
 
-    public Echipa(String n, int m){
+    public Echipa(String n, int m) {
         nume = n;
         MAX = m;
     }
 
-    public boolean addMember(Membru member){
-        if(nr >= MAX)
+    public boolean addMember(Membru member) {
+        if (nr >= MAX)
             return false;
 
         membri.add(member);
         return true;
     }
 
-    public boolean setLeader(Membru liderNou){
-        if(liderNou.getExp() < 5)
+    public boolean setLeader(Membru liderNou) {
+        if (liderNou.getExp() < 5)
             return false;
 
         lider = liderNou;
         return true;
     }
 
-    public Membru removeMember(Membru member){
-        if(membri.remove(member))
+    public Membru removeMember(Membru member) {
+        if (membri.remove(member))
             return member;
         return null;
     }
 
-    public String toString(){
+    public String toString() {
         String aux = "";
 
-        for (Membru m: membri) {
-            aux = aux + m.toString();
+        for (Membru m : membri) {
+            aux = aux + " " + m.toString();
         }
         return "Lider echipa: " + lider.toString() + ", Membri: " + aux;
     }
 
-    public void setMax(int m){
-        if(this.equals(lider)){
+    public void setMax(int m) {
+        if (this.equals(lider)) {
             MAX = m;
-        }
-        else {
+        } else {
             System.out.println("Doar liderul are voie sa schimbe nr max de membri!");
         }
     }
 
-    public void setName(String n){
-        if(this.equals(lider)){
+    public void setName(String n) {
+        if (this.equals(lider)) {
             nume = n;
-        }
-        else {
+        } else {
             System.out.println("Doar liderul are voie sa schimbe numele echipei!");
         }
     }
@@ -64,14 +62,53 @@ public abstract class Echipa {
     public abstract double getCost();
 }
 
-class DevTeam extends Echipa{
-    public double getCost() {
+class DevTeam extends Echipa {
+    public DevTeam(String n, int m) {
+        super(n, m);
+    }
 
+    private double getCostM(Membru m){
+        int exp = m.getExp();
+        if(m.equals(lider))
+            return (double) 250 * (2500 + exp * 250);
+        if (exp >= 2 && exp <= 5)
+            return (double) 1500 + (double) 25 / 100 * 1500;
+        else if (exp > 5)
+            return (double) 1500 + (double) 50 / 100 * 1500;
+        return 1500.0;
+    }
+
+    public double getCost() {
+        double s = 0.0;
+        for (Membru m : membri) {
+            s = s + getCostM(m);
+        }
+        return s;
     }
 }
 
-class HR extends Echipa{
+class HR extends Echipa {
+    public HR(String n, int m) {
+        super(n, m);
+    }
+
+    private double getCostM(Membru m) {
+        int exp = m.getExp();
+
+        if(m.equals(lider))
+            return (double) 1350 + exp * 300;
+        if (exp >= 2 && exp <= 5)
+            return (double) 1000 + (double) 25 / 100 * 1000;
+        else if (exp > 5)
+            return (double) 1000 + (double) 50 / 100 * 1000;
+        return 1000.0;
+    }
+
     public double getCost() {
-        return 0;
+        double s = 0.0;
+        for (Membru m : membri) {
+            s = s + getCostM(m);
+        }
+        return s;
     }
 }
--- Exercitii/src/Main.java
+++ Exercitii/src/Main.java
@@ -1,3 +1,44 @@
 public class Main {
+    public static void main(String[] args){
+        DevTeam dv = new DevTeam("Naruto", 5);
+        HR hr = new HR("Sasuke", 6);
 
+        Membru ninja1 = new Membru("Sakura", 31, 100, 10 );
+        Membru ninja2 = new Membru("Boruto", 13, 200, 1 );
+        Membru ninja3 = new Membru("Sarada", 13, 300, 1 );
+        Membru ninja4 = new Membru("Mitsuki", 12, 400, 2 );
+        Membru ninja5 = new Membru("Himawari", 10, 500, 0 );
+        Membru ninja6 = new Membru("Hinata", 32, 600, 11 );
+        Membru ninja7 = new Membru("Sakura1", 31, 700, 10 );
+        Membru ninja8 = new Membru("Boruto1", 13, 800, 1 );
+        Membru ninja9 = new Membru("Sarada1", 13, 900, 1 );
+        Membru ninja10 = new Membru("Mitsuki1", 12, 1000, 2 );
+        Membru ninja11 = new Membru("Himawari1", 10, 1100, 0 );
+        Membru ninja12 = new Membru("Hinata1", 33, 1200, 6);
+
+        System.out.println(dv.setLeader(ninja2));
+        System.out.println(dv.setLeader(ninja6));
+        dv.addMember(ninja2);
+        dv.addMember(ninja5);
+        dv.addMember(ninja8);
+        dv.addMember(ninja11);
+        dv.addMember(ninja4);
+
+        hr.setLeader(ninja1);
+        hr.addMember(ninja3);
+        hr.addMember(ninja12);
+        hr.addMember(ninja7);
+        hr.addMember(ninja9);
+        hr.addMember(ninja10);
+
+        System.out.println(dv.getCost());
+        System.out.println(hr.getCost());
+
+        System.out.println(dv.toString());
+        System.out.println(hr.toString());
+
+        System.out.println(dv.removeMember(ninja4));
+        System.out.println(dv.toString());
+        System.out.println(dv.getCost());
+    }
 }
